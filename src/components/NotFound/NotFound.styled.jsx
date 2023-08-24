b1ebf74ed707c25864468ee5a580ba231b4cc857
Andrii Veremii <dashuk10@gmail.com>
2023-08-24 23:45:26 +0200

    add NotFaund
    -додав основні файли
    -додав картинкі

---
+++ src/components/NotFound/NotFound.styled.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+
+
+export const Wrapper = styled.section`
+  
+`;
+
+export const Text = styled.p`
+  
+`;
+
+export const Img = styled.img`
+  
+`;
+export const Info = styled.div`
+  
+`;
+export const Home = styled(Link)`
+  
+`;
