d26e4c93105d1cfa06779651e9313307194d9ec8
const604 <116814907+const604@users.noreply.github.com>
2023-08-24 13:12:26 +0300

    modal

---
+++ src/components/Modal/Modal.styled.jsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 1200;
+`;
+
+export const ModalContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
