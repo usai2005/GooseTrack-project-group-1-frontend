1e0154064bb8029933738726083f549f2475279b
ZubkoA <114995045+ZubkoA@users.noreply.github.com>
2023-08-25 18:49:24 +0300

    start

--- src/components/Form/FormTemplate/FormTemplate.js
+++ src/components/Form/FormTemplate/FormTemplate.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
 import FormRegister from '../FormRegister/FormRegister';
 import FormTitle from '../FormTitle/FormTitle';
-import FormButton from '../FormButton/FormButton';
 import { Container, ContainerForm } from './FormTemplate.styled';
 
 const FormTemplate = () => {
