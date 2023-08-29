d429aa13718bf65a817319cd4c1781020ee3ce45
Daria Tkachenko <113775461+dariaaferreira@users.noreply.github.com>
2023-08-29 23:31:24 +0300

    added redux logout logic

--- src/components/SideBar/LogoutBtn/LogoutBtn.jsx
+++ src/components/SideBar/LogoutBtn/LogoutBtn.jsx
@@ -1,13 +1,21 @@
+import { useDispatch } from 'react-redux';
 import icons from '../../../images/icons.svg';
 
 import {
     LogoutLink,
     LogoutIcon,
 } from './LogoutBtn.styled';
+import { logOut } from 'redux/auth/operations';
 
 const LogoutBtn = () => {
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch(logOut());
+    };
+
     return (
-        <LogoutLink>
+        <LogoutLink onClick={handleLogout}>
             <span>Log out</span>
             <LogoutIcon>
                 <use href={icons + '#icon-log-out-01'}></use>
