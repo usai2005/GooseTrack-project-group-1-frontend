52e6e678847c233ef1ad951596d7f13ba69b930c
usai2005 <88884179+usai2005@users.noreply.github.com>
2023-09-05 14:37:59 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,28 @@
 import AddFeedbackBt from '../AddFeedbackBtn/AddFeedbackBtn';
 import ThemeToggler from '../ThemeToggler/ThemeToggler';
 import UserInfo from '../UserInfo/UserInfo';
-// import {Container} from '../../styles/container'
+import IMAGES from '../../images/sideBar/index'
 import icons from '../../images/icons.svg';
 
 import {
   ContainerHeader,
   MenuBtn,
   MenuIcon,
+  ImgHeaderDay,
   PageTitle,
+  TextHeader,
 } from './Header.styled';
 
 import {
   useState,
-  // useEffect
 } from 'react';
-// import { useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 import { toggleSideBar } from '../../redux/side-bar/sideBarSlice';
 
 import { FeedbackModal } from 'components/Feedback/FeedbackModal';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectUser } from 'redux/auth/selectors';
-
-let pageTitle = 'User Profile';
 
 const Header = () => {
   const [isOpened, setIsOpen] = useState(false);
@@ -37,19 +36,17 @@ const Header = () => {
     dispatch(toggleSideBar());
   };
 
-  // const { pathname } = useLocation();
+  const { pathname } = useLocation();
   
-  // useEffect(() => {
-  //   if (pathname.includes('/statistics')) {
-  //     pageTitle = 'Statistics';
-  //     return;
-  //   } else if (pathname.includes('/account')) {
-  //     pageTitle = 'User Profile';
-  //   } else {
-  //     pageTitle = 'Calendar';
-  //   }
-  // }, [pathname]);
-
+  let pageTitle = '';
+  
+  if (pathname.includes('/statistics')) {
+    pageTitle = 'Statistics';
+  } else if (pathname.includes('/account')) {
+    pageTitle = 'User Profile';
+  } else {
+    pageTitle = 'Calendar';
+  };
 
   const user = useSelector(selectUser);
 
@@ -63,7 +60,19 @@ const Header = () => {
           <use href={icons + '#icon-menu-01'}></use>
         </MenuIcon>
       </MenuBtn>
-      <PageTitle>{ pageTitle }</PageTitle>
+      {pathname.includes('/calendar/day') && (<ImgHeaderDay
+        srcSet={`${IMAGES.headerGooseDesktop} 64w,
+          ${IMAGES.headerGooseDesktop2x} 128w`}
+            src={IMAGES.headerGooseDesktop}
+            alt="goos of header"
+            loading="lazy"
+      />)}
+      <div>
+        <PageTitle>{pageTitle}</PageTitle>
+        {pathname.includes('/calendar/day') && (<TextHeader>
+          <span>Let go </span>of the past and focus on the present!
+        </TextHeader>)}
+      </div>
       <AddFeedbackBt setIsOpen={setIsOpen} />
       <ThemeToggler />
       <UserInfo user={user}/>
@@ -71,6 +80,7 @@ const Header = () => {
         <FeedbackModal onClose={handleToggle} user={user}></FeedbackModal>
       )}
     </ContainerHeader>
+
   );
 };
 
