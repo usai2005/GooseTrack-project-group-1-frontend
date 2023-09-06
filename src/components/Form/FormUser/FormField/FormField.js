41a2309589e4e404027f48d7c2455ec48eac6fe1
Daria Tkachenko <113775461+dariaaferreira@users.noreply.github.com>
2023-09-06 17:25:46 +0300

    fixed user validation inputs

--- src/components/Form/FormUser/FormField/FormField.js
+++ src/components/Form/FormUser/FormField/FormField.js
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 import InputMask from 'react-input-mask';
 
 // import { ErrorMessage } from '@hookform/error-message';
-
 // import {StyledErrorMessage} from '../consts/styledErrorMessage.styled'
 
 import { Label, Input, InputFieldThumb } from './FormField.styled';
@@ -14,50 +13,54 @@ export const FormField = ({
   placeholder,
   label,
   register,
-  // errors,
   setIsDisabled,
+  error,
 }) => {
   const isPhoneField = inputName === 'phone';
 
   return (
     <>
-    <InputFieldThumb>
-      <Label htmlFor={id}>{label}</Label>
-      {isPhoneField ? (
-        <InputMask
-          id={id}
-          mask="+99 (999) 999-9999"
-          maskChar="_"
-          type="tel"
-          placeholder={placeholder}
-          {...register(inputName)}
-          onChange={() => {
-            setIsDisabled(false);
-          }}
-        >
-          {(inputProps) => (
-            <Input {...inputProps} />
-          )}
-        </InputMask>
-      ) : (
-        <Input
-          id={id}
-          type={type}
-          placeholder={placeholder}
-          {...register(inputName)}
-          onChange={() => {
-            setIsDisabled(false);
-          }}
-        />
+      <InputFieldThumb>
+        <Label htmlFor={id}>{label}</Label>
+        {isPhoneField ? (
+          <InputMask
+            id={id}
+            mask="+99 (999) 999-9999"
+            maskChar="_"
+            type="tel"
+            placeholder={placeholder}
+            {...register(inputName)}
+            onChange={() => {
+              setIsDisabled(false);
+            }}
+          >
+            {(inputProps) => (
+              <Input {...inputProps} />
+            )}
+          </InputMask>
+        ) : (
+          <Input
+            id={id}
+            type={type}
+            placeholder={placeholder}
+            {...register(inputName)}
+            onChange={() => {
+              setIsDisabled(false);
+            }}
+          />
+        )}
+      </InputFieldThumb>
+      {error && (
+        <p style={{ color: 'red' }}>{error.message}</p>
       )}
-      
-    </InputFieldThumb>
-    {/* <StyledErrorMessage>
+
+      {/* <StyledErrorMessage>
         <ErrorMessage errors={errors} name={inputName} />
       </StyledErrorMessage> */}
-</>
+    </>
   );
 };
+
 
 FormField.propTypes = {
   id: PropTypes.string.isRequired,
@@ -66,5 +69,5 @@ FormField.propTypes = {
   placeholder: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   register: PropTypes.func.isRequired,
-  // errors: PropTypes.any.isRequired,
+  error: PropTypes.any.isRequired,
 };
--- src/components/Form/FormUser/consts/validation/FormUserSchema.js
+++ src/components/Form/FormUser/consts/validation/FormUserSchema.js
@@ -1,29 +1,54 @@
 import * as Yup from 'yup';
 
-import { isValidFileType } from './fileTypeValidation';
+// import { isValidFileType } from './fileTypeValidation';
 import { nameRegExp, phoneRegExp } from './regExps';
 
-
+import moment from 'moment/moment';
+const dayMoment = moment().format('DD/MM/YYYY');
 
 export const FormUserSchema = Yup.object().shape({
   name: Yup.string()
-    .required('This field is required')
+    .required('Name is required')
     .matches(
       nameRegExp,
       "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
     )
-    .max(16, 'Name may contain only 16 characters'),
-  phone: Yup.string().matches(phoneRegExp, {
-    message:
-      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +',
-    excludeEmptyString: true,
-  }),
-  birthday: Yup.date('Invalid date format'),
-  skype: Yup.string().max(16, 'Skype may contain only 16 characters'),
+    .max(20, 'At most 16 digits is required')
+    .test(
+      'name-validation',
+      'Name must be at least 3 characters long',
+      value => {
+        return value && value.replace(/\s/g, '').length >= 3;
+      }
+    ),
+  birthday: Yup.date().max(
+    dayMoment,
+
+    'Birthday must be earlier than today'
+  ),
   email: Yup.string()
-    .email('Invalid email format')
-    .required('This field is required'),
-  avatarURL: Yup.mixed().test('is-valid-type', 'Ivalid image type', value => {
-    return value === '' || isValidFileType(value);
-  }),
+    .email('This is an ERROR email')
+    .matches(/^[a-zA-Z0-9@.]+$/, 'Email must contain only Latin characters')
+    .required('Email is required field'),
+  phone: Yup.string()
+    .required(
+      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+    )
+    .matches(
+      phoneRegExp.phone,
+      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+    ),
+  skype: Yup.string()
+    .matches(/^\S[\S\s]{0,28}\S$/, 'Skype must be between 3 and 16 characters')
+    .max(13, 'At most 13 digits is required')
+    .test(
+      'Skype-validation',
+      'Skype must be at least 3 characters long',
+      value => {
+        return value && value.replace(/\s/g, '').length >= 3;
+      }
+    ),
+    // avatarURL: Yup.mixed().test('is-valid-type', 'Ivalid image type', value => {
+    //   return value === '' || isValidFileType(value);
+    // }),
 });
